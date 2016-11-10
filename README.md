4ad0f6c088fc0bb03e0a254c89ea86dcb86f20a1
Daniel Lucas <daniel.r.lucas@gmail.com>
2016-11-10 17:01:10 -0500

    OCD-887: comments, console input&output, javadoc

--- README.md
+++ README.md
@@ -15,16 +15,21 @@ Using Git Bash, run the following command:
 $ git clone https://github.com/chpladmin/chpl-api.git
 ```
 
-## Authentication token & testing databases
+## Add external JARs to project
 
-The JSON token definition needs to be set in a file named `chpl/chpl-api-for-dummies/src/main/resources/environment.auth.properties`. There is a file named `chpl/chpl-auth/src/main/resources/environment.auth.properties.template` that has the format. Copy that file and change the `keyLocation` key to something local.
-
-Two files are used for testing purposes. There are template files for each of them that will need to be copied and renamed, with valid local data inserted:
-
-```
-chpl/chpl-auth/src/test/resources/environment.auth.test.properties
-chpl/chpl-api-for-dummies/src/test/resources/environment.test.properties
-```
+fluent-hc-4.5.2.jar
+java-json.jar
+gson-2.8.0.jar
+log4j-api-2.6.2.jar
+httpclient-4.5.2.jar
+httpcore-4.4.4.jar
+commons-logging-1.2.jar
+commons-csv-1.2.jar
+commons-codec-1.9.jar
+commons-lang3-3.1.jar
+commons-io-2.5.jar
+httpmime-4.5.2.jar
+httpclient-win-4.5.2.jar
 
 # Calling the REST API
 Let's walk through the different parts of a sample HTTP GET REST call, which can be done programmatically through Java code or an application such as Postman.
@@ -37,4 +42,4 @@ Headers:
 API-key: 12909a978483dfb8ecd0596c98ae9094
 Content-Type: application/json
 
-When calling the API programmatically, it is best practice to define sensitive information, including the API-key, username and password, via a local environment.properties file. Within the SampleApp GitHub, there is an environment.properties.template example file. Rename this file to environment.properties, and update the parameter values as appropriate.
+When calling the API programmatically, it is best practice to define sensitive information, including the API-key, username and password, via a local environment.properties file. Within the SampleApp GitHub, there is an environment.properties.template file. Rename this file to environment.properties, and update the parameter values as appropriate.
--- src/main/Java/SampleApp.java
+++ src/main/Java/SampleApp.java
@@ -2,11 +2,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.util.Properties;
-
-import javax.xml.bind.PropertyException;
+import java.util.Scanner;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpVersion;
@@ -37,8 +35,11 @@ public class SampleApp {
 	
 	public static void main(String[] args) throws IOException, URISyntaxException, JSONException {
 		SampleApp sampleApp = new SampleApp();
+		String token = null;
 		
 		props = sampleApp.loadProperties(DEFAULT_PROPERTIES_FILE);
+		sampleApp.displayInstructions();
+		Integer input = sampleApp.getValidUserInput();
 		
 		/**
 		 * Example 1:
@@ -46,50 +47,129 @@ public class SampleApp {
 		 * b. Parse JSON response using com.google.gson
 		 * c. Return token as string
 		 */
-		String token = sampleApp.getToken();
+		if(input == 0 || input == 1 || input == 4){
+			// This token could be used in an API call that requires authentication. 
+			token = sampleApp.getToken();
+		}
 		
 		/**
 		 * Example 2: 
 		 * a. Call the API /search using HTTP GET to get all Certified Product Details
-		 * b. Parse the JSON using org.json to get only information keyed on "product".
+		 * b. Parse the JSON using org.json to get only information keyed on "product"
 		 * c. Parse the "product" JSON to CSV format
 		 * d. Write the csv data to a file
 		 */
-		String searchResult = sampleApp.getSearchResult();
-		JSONObject search = new JSONObject(searchResult);
-		JSONArray searchArr = search.getJSONArray("results");
-		JSONObject products = new JSONObject();
-		// Get product JSONObject with the CP_id as "id", then the "product" names/values: "versionId", "name", "id", "version"
-		for (Integer i = 0; i < searchArr.length(); i++) {
-		      if(searchArr.optJSONObject(i).has("product")){
-		    	  products.put(searchArr.optJSONObject(i).get("id").toString(), searchArr.optJSONObject(i).get("product"));
-		      }
-		 }
-
-		String csv = CDL.toString(products.toJSONArray(products.names()));
-		File file = new File("products.csv");
-		FileUtils.writeStringToFile(file, csv);
+		if(input == 0 || input == 2){
+			String searchResult = sampleApp.getSearchResult();
+			JSONObject search = new JSONObject(searchResult);
+			JSONArray searchArr = search.getJSONArray("results");
+			JSONObject products = new JSONObject();
+			
+			for (Integer i = 0; i < searchArr.length(); i++) {
+			      if(searchArr.optJSONObject(i).has("product")){
+			    	  products.put(searchArr.optJSONObject(i).get("id").toString(), searchArr.optJSONObject(i).get("product"));
+			      }
+			 }
+			
+			System.out.println("Parsed search JSONArray to retrieve products as follows: \n" + products.toString());
+			JSONArray productNames = products.toJSONArray(products.names());
+			System.out.println("Parsed certified products JSON to get products as follows: \n" + productNames.toString());
+			String csv = CDL.toString(productNames);
+			System.out.println("Parsed products JSON to CSV as follows: \n" + csv);
+			File file = new File("products.csv");
+			FileUtils.writeStringToFile(file, csv);
+			System.out.println("Saved product information to products.csv");
+		}
 		
 		/**
 		 * Example 3: 
 		 * a. call the API /search to get all Certified Product Details
 		 * b. Save results to a .txt file
 		 */
-		sampleApp.getSearchResult_saveResultsInFile();
+		if(input == 3 || input == 0){
+			sampleApp.getSearchResult_saveResultsInFile();
+		}
+		
+		/**
+		 * Example 4:
+		 * a. Call the API /data/search_options using token for authentication
+		 */
+		if(input == 4 || input == 0){
+			sampleApp.getSearchOptions_usesAuthentication(token);
+		}
+		System.out.println("Completed execution. Exiting application...");
 	}
 	
 	/**
-	 * Uses HTTP POST to get an authenticated token to be passed for future API calls that require an authenticated user.
+	 * Displays instructions on how to use the CHPL Sample Application
+	 */
+	public void displayInstructions(){
+		System.out.println("Welcome to the CHPL Sample Application! The purpose of this application is to provide the following examples: \n");
+		
+		System.out.println("Example 1: ");
+		System.out.println("a. Call the API /auth/authentication using HTTP POST to get a valid token");
+		System.out.println("(note: the authentication token is not necessary for many API calls, including /search)");
+		System.out.println("b. Parse JSON response using com.google.gson");
+		System.out.println("c. Return token as string\n");
+		
+		System.out.println("Example 2: ");
+		System.out.println("a. Call the API /search using HTTP GET to get all Certified Product Details");
+		System.out.println("b. Parse the JSON using org.json to get only information keyed on \"product\"");
+		System.out.println("c. Parse the \"product\" JSON to CSV format");
+		System.out.println("d. Write the csv data to a file\n");
+		
+		System.out.println("Example 3: ");
+		System.out.println("a. call the API /search to get all Certified Product Details");
+		System.out.println("b. Save results to a .txt file\n");
+		
+		System.out.println("Example 4: ");
+		System.out.println("a. Call the API /data/search_options using token for authentication\n");
+		
+		System.out.println("NOTE: before running, please configure the environment.properties file\n");
+		
+		System.out.println("For example 1, please enter '1' into the console");
+		System.out.println("For example 2, please enter '2' into the console");
+		System.out.println("For example 3, please enter '3' into the console");
+		System.out.println("For example 4, please enter '4' into the console");
+		System.out.println("To perform all 4 examples, please enter '0' into the console");
+		System.out.println("To exit the application, please enter '9' into the console\n");
+	}
+	
+	/**
+	 * Returns a valid integer from the console
+	 * @return integer representing user input
+	 * @throws IOException for incorrect user console input
+	 */
+	public Integer getValidUserInput() throws IOException{
+		Scanner scanner = new Scanner(System.in);
+		Integer inputInt = scanner.nextInt();
+		if(inputInt.equals(null)){
+			System.out.println("User input integer is null. Setting to 0 as default.");
+			inputInt = 0;
+		}
+		else if(inputInt == 9){
+			System.out.println("Exiting application...");
+			System.exit(0);
+		}
+		else if(inputInt != 0 && inputInt != 1 && inputInt != 2 && inputInt != 3 && inputInt != 4){
+			System.out.println("Invalid user input. Please enter an integer between 0 and 4.");
+			System.exit(0);
+		}
+		scanner.close();
+		return inputInt;
+	}
+	
+	/**
+	 * Uses HTTP POST to call /auth/authenticate and get an authenticated token to be passed for future API calls that require an authenticated user.
 	 * Uses the Properties from environment.properties to obtain username, password, and API-key
 	 * Parses json response and returns the value of the token from the json
-	 * @return
-	 * @throws IOException 
-	 * @throws ClientProtocolException 
-	 * @throws URISyntaxException
-	 * @throws PropertyException 
-	 * @throws UnsupportedEncodingException 
+	 * @return String with the token
+	 * @throws IOException for REST HTTP POST
 	 */
-	public String getToken() throws ClientProtocolException, IOException {
+	public String getToken() throws IOException {
+		System.out.println("\nRunning example 1 to get token for authentication:");
+		System.out.println("Making REST HTTP POST call to " + props.getProperty("targetHost") + props.getProperty("authenticate") + 
+				" using API-key=" + props.getProperty("apiKey").substring(0, 8) + "...");
 		String tokenResponse = Request.Post(props.getProperty("targetHost") + props.getProperty("authenticate"))
 		.bodyString("{ \"userName\": \"" + props.getProperty("username") + "\","
 				+ " \"password\": \"" + props.getProperty("password") + "\" }", ContentType.APPLICATION_JSON)
@@ -98,30 +178,38 @@ public class SampleApp {
 		.addHeader("API-key", props.getProperty("apiKey"))
 		.execute().returnContent().asString();
 		JsonObject jobj = new Gson().fromJson(tokenResponse, JsonObject.class);
+		System.out.println("Retrieved the following JSON from /auth/authenticate: \n" + jobj.toString());
 		String token = jobj.get("token").toString();
+		System.out.println("Retrieved token " + token);
 		return token;
 	}
 	
 	/**
 	 * Uses HTTP GET to obtain the JSON response as a string from the /search?searchTerm= API call
-	 * @throws IOException 
-	 * @throws ClientProtocolException 
+	 * @throws IOException for REST HTTP GET
+	 * @return String with the searchResult
 	 */
-	public String getSearchResult() throws ClientProtocolException, IOException{
+	public String getSearchResult() throws IOException{
+		System.out.println("\nRunning example 2 to search for certified products:");
+		System.out.println("Making REST HTTP GET call to " + props.getProperty("targetHost") + props.getProperty("search") + 
+				" using API-key=" + props.getProperty("apiKey").substring(0, 8) + "...");
 		String searchResult = Request.Get(props.getProperty("targetHost") + props.getProperty("search"))
 				.version(HttpVersion.HTTP_1_1)
 				.addHeader("Content-Type", "application/json")
 				.addHeader("API-key", props.getProperty("apiKey"))
 				.execute().returnContent().asString();
+		System.out.println("Retrieved " + props.getProperty("targetHost") + props.getProperty("search") + " JSON as follows: \n" + searchResult);
 		return searchResult;
 	}
 
 	/**
 	 * Uses HTTP GET to obtain the JSON response from the /search?searchTerm= API call and save as a csv
-	 * @throws IOException 
-	 * @throws ClientProtocolException 
+	 * @throws IOException for REST HTTP GET
 	 */
-	public void getSearchResult_saveResultsInFile() throws ClientProtocolException, IOException{
+	public void getSearchResult_saveResultsInFile() throws IOException{
+		System.out.println("\nRunning example 3 to search for certified products and save JSON in a text file:");
+		System.out.println("Making REST HTTP GET call to " + props.getProperty("targetHost") + props.getProperty("search") + 
+				" using API-key=" + props.getProperty("apiKey").substring(0, 8) + "..." + " and saving JSON to fullResults.txt");
 		Request.Get(props.getProperty("targetHost") + props.getProperty("search"))
 				.version(HttpVersion.HTTP_1_1)
 				.addHeader("Content-Type", "application/json")
@@ -130,29 +218,50 @@ public class SampleApp {
 	}
 	
 	/**
-	 * Utility method to load Properties from environment.properties file.
-	 * @param filePathAndName
-	 * @return
+	 * Uses HTTP GET to obtain the JSON response from the /data/search_options API call. Uses token for authentication
+	 * @param token for use in API authentication calls
+	 * @throws IOException for REST HTTP GET
 	 */
-	public Properties loadProperties(
-		      final String filePathAndName)
-		   {
-		      final Properties properties = new Properties();
-		      try
-		      {
-		         final InputStream in = SampleApp.class.getClassLoader().getResourceAsStream(DEFAULT_PROPERTIES_FILE);
-		         properties.load(in);
-		         in.close();
-		      }
-		      catch (FileNotFoundException fnfEx)
-		      {
-		         System.err.println("Could not read properties from file " + filePathAndName);
-		      }
-		      catch (IOException ioEx)
-		      {
-		         System.err.println(
-		            "IOException encountered while reading from " + filePathAndName);
-		      }
-		      return properties;
-		   }
+	public void getSearchOptions_usesAuthentication(String token) throws IOException{
+		System.out.println("\nRunning example 4 to call /data/search_options using a token for authentication: ");
+		if(token == null){
+			System.out.println("Token is null. Please provide a valid token for authentication.");
+			System.exit(0);
+		}
+		System.out.println("Making REST HTTP GET call to " + props.getProperty("targetHost") + props.getProperty("searchOptions") + 
+				" using API-key=" + props.getProperty("apiKey").substring(0, 8) + "...");
+		String searchResult = Request.Get(props.getProperty("targetHost") + props.getProperty("searchOptions"))
+				.version(HttpVersion.HTTP_1_1)
+				.addHeader("Content-Type", "application/json")
+				.addHeader("API-key", props.getProperty("apiKey"))
+				.addHeader("Authorization", "Bearer " + token)
+				.execute().returnContent().asString();
+		System.out.println("Retrieved result of " + props.getProperty("targetHost") + props.getProperty("searchOptions") + " as follows: \n" + searchResult);
+	}
+	
+	/**
+	 * Utility method to load Properties from environment.properties file.
+	 * @param filePathAndName location of environment.properties file
+	 * @return Properties necessary for user credentials, API-key and API calls
+	 */
+	public Properties loadProperties(final String filePathAndName){
+      final Properties properties = new Properties();
+      try
+      {
+         final InputStream in = SampleApp.class.getClassLoader().getResourceAsStream(DEFAULT_PROPERTIES_FILE);
+         properties.load(in);
+         in.close();
+         System.out.println("Loaded Properties from Environment.properties");
+      }
+      catch (FileNotFoundException fnfEx)
+      {
+         System.err.println("Could not read properties from file " + filePathAndName);
+      }
+      catch (IOException ioEx)
+      {
+         System.err.println(
+            "IOException encountered while reading from " + filePathAndName);
+      }
+      return properties;
+   }
 }
